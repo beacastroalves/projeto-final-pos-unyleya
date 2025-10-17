858c75318173847557a23bac75b1e0fd80035dcd
beacastroalves <beacastro1008@gmail.com>
2025-10-17 18:53:33 +0100

    criando o primeiro componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import Header from "./components/header";
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-center text-lg bg-primary font-ubuntu">Hello World</h1>
-      <h1 className="text-center text-lg font-bold bg-secondary">Hello World</h1>
+      <Header/>
     </div>
   )
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div className="bg-primary flex justify-between items-center py-4 px-3">
+      <h1 className="text-white text-[32px]">UnyBay</h1>
+
+      <ul className="flex gap-5 items-center text-white">
+        <li className="cursor-pointer">Home</li>
+        <li className="cursor-pointer">Quem Somos</li>
+        <li>
+          <button className="bg-secondary px-8 py-2 rounded-md cursor-pointer">Entrar</button>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header;
