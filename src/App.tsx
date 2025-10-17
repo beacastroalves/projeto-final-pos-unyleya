d4d15343636d18a8b3078f5782482276dfa19139
beacastroalves <beacastro1008@gmail.com>
2025-10-17 18:43:46 +0100

    configurando e testando o tema da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-center text-lg font-bold bg-blue-500">Hello World</h1>
+      <h1 className="text-center text-lg bg-primary font-ubuntu">Hello World</h1>
+      <h1 className="text-center text-lg font-bold bg-secondary">Hello World</h1>
     </div>
   )
 }
