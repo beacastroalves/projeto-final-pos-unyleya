6d83631a45c6a7c4c5f37d0ced094e31288efea0
beacastroalves <beacastro1008@gmail.com>
2025-10-23 18:12:15 +0100

    Adicionando a pagina de busca

--- src/pages/search-result/index.tsx
+++ src/pages/search-result/index.tsx
@@ -1,9 +1,20 @@
+import CardProduct from "../../components/card-product";
 import UserTemplate from "../../templates/user-template";
 
 const SearchProducts = () => {
   return (
     <UserTemplate>
-      <h1>Search Result - Page</h1>
+      <h1 className="mt-10 m-4 text-[20px]">Resultado da busca</h1>
+
+      <div className="flex flex-wrap justify-between">
+        {
+          Array.from({ length: 4 }).map(() => (
+            <CardProduct />
+          ))
+        }
+      </div>
+
+      <p className="self-end m-4">4 itens</p>
     </UserTemplate>
   )
 };
