d7b99e435cc62a9280ee37a0065e75dbad5df8eb
beacastroalves <beacastro1008@gmail.com>
2025-10-25 19:05:27 +0100

    Ajustes no footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,14 +1,25 @@
-import {BsLinkedin, BsFacebook} from "react-icons/bs";
+import {BsLinkedin, } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+
+  const navigate = useNavigate();
+
   return (
     <footer className="text-white bg-primary p-10">
-      <h2 className="text-[22px] font-bold mb-5">Unybay</h2>
+      <button onClick={() => navigate('/')} className="cursor-pointer transition duration-200 hover:text-shadow-lg">
+        <h2 className="text-[22px] font-bold mb-5">Unybay</h2>
+      </button>
 
       <p className="text-center">Unyleya Educacional | Todos os direitos reservados </p>
-      <div className="flex justify-center gap-2 mt-5">
-        <BsLinkedin />
-        <BsFacebook />
+      <div className="flex justify-center gap-3 mt-5">
+        <a href="https://www.linkedin.com/in/beacastrodev/" target="_blank" className="cursor-pointer scale-110 transition duration-250 hover:scale-160" title="Linkedin - Beatriz Castro">
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/beacastroalves" target="_blank" className="cursor-pointer scale-115 transition duration-250 hover:scale-170" title="Github Page - Beatriz Castro">
+          <FaGithub />
+        </a>
       </div>
     </footer>
   )
