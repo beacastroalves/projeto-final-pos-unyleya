c1f4062738dfb1806fd3273fa5de86d117794501
beacastroalves <beacastro1008@gmail.com>
2025-10-28 12:42:45 +0000

    Implementando a pagina dashboard e componente header-admin

---
+++ src/components/header-admin/index.tsx
@@ -0,0 +1,36 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const HeaderAdmin = () => {
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="bg-primary flex justify-between items-center py-4 px-3">
+      <button onClick={() => navigate('/')} className="cursor-pointer">
+        <h1 className="text-white text-[32px] font-bold">Unybay</h1>
+      </button>
+
+      <ul className="flex gap-5 items-center text-white">
+        <li className="cursor-pointer py-2 px-3 rounded-md transition duration-300 hover:bg-primary-300 active:bg-primary-600 active:translate-[1px] active:drop-shadow-sm">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="cursor-pointer py-2 px-3 rounded-md transition duration-300 hover:bg-primary-300 active:bg-primary-600  active:translate-[1px] active:drop-shadow-sm">
+          <Link to="/about-us" className="text-white">Quem Somos</Link>
+        </li>
+        <li className="cursor-pointer py-2 px-3 rounded-md transition duration-300 hover:bg-primary-300 active:bg-primary-600  active:translate-[1px] active:drop-shadow-sm">
+          <Link to="/" className="text-white">Sair</Link>
+        </li>
+        <li>
+          <button
+            onClick={() => navigate('#')}
+            className="bg-white text-secondary px-8 py-2 ml-2 rounded-md cursor-pointer transition duration-300 hover:drop-shadow-md hover:text-secondary-300 active:text-secondary-600 active:translate-[1px] active:drop-shadow-lg"
+          >
+            Anunciar
+          </button>
+        </li>
+      </ul>
+    </div>
+  )
+};
+
+export default HeaderAdmin;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,112 @@
 import AdminTemplate from "../../templates/admin-template";
+import CardProduct from "../../components/card-product";
+
+import { LuGamepad2 } from "react-icons/lu";
+import { AiOutlineCar, AiOutlineGift, AiOutlineSync } from "react-icons/ai";
+import { IoFastFoodOutline, IoSearch, IoShirtOutline } from "react-icons/io5";
+import { VscTools } from "react-icons/vsc";
+
+import { Carousel } from "react-responsive-carousel";
+import carousel1 from "../../assets/images/carousel1.jpg";
+import carousel2 from "../../assets/images/carousel2.jpg";
+import carousel3 from "../../assets/images/carousel3.jpg";
+
+import { useNavigate } from "react-router-dom";
+
+const itemsCategory = [
+  {
+    id: 0,
+    title: "Jogos",
+    icon: <LuGamepad2 />
+  },
+  {
+    id: 1,
+    title: "Roupas",
+    icon: <IoShirtOutline />
+  },
+  {
+    id: 2,
+    title: "Veículos",
+    icon: <AiOutlineCar />
+  },
+  {
+    id: 3,
+    title: "Ferramentas",
+    icon: <VscTools />
+  },
+  {
+    id: 4,
+    title: "Comidas",
+    icon: <IoFastFoodOutline />
+  },
+  {
+    id: 5,
+    title: "Presentes",
+    icon: <AiOutlineGift />
+  },
+  {
+    id: 6,
+    title: "Outros",
+    icon: <AiOutlineSync />
+  }
+];
 
 const Dashboard = () => {
+
+  const navigate = useNavigate();
+
   return (
     <AdminTemplate>
-      <h1>Admin Template Dashboard</h1>
+      <div className="mt-10 mb-10">
+        <Carousel showThumbs={false} autoPlay={true} emulateTouch={true} infiniteLoop={true}>
+          <div>
+            <img src={carousel1} alt="Imagem de algo 1" />
+          </div>
+          <div>
+            <img src={carousel2} alt="Imagem de algo 2" />
+          </div>
+          <div>
+            <img src={carousel3} alt="Imagem de algo 3" />
+          </div>
+        </Carousel>
+      </div>
+
+      <div className="flex justify-between items-center gap-2 rounded-md shadow-md p-3 px-6 bg-white border-2 border-gray-50">
+        <input type="text" className="w-full outline-none" placeholder="Estou buscando por..." />
+        <button onClick={() => navigate('/products/search')} className="px-2 cursor-pointer py-1 transition duration-250 hover:brightness-70 active:brightness-40 active:translate-[1px]">
+          <IoSearch className="text-[24px] text-gray-400"/>
+        </button>
+      </div>
+
+      <h2 className="mt-12">Itens recentes</h2>
+      <div className="flex flex-wrap">
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+      </div>
+      <p className="flex self-end">Ver mais</p>
+
+      <div className="bg-primary px-8 py-4 pb-8 rounded-lg mt-10 mb-10 shadow-lg">
+        <h2 className="text-white text-[24px] mb-6">Categorias</h2>
+        <div className="flex justify-around">
+          {itemsCategory.map((category) => (
+            <div className="flex flex-col  items-center cursor-pointer">
+              <div className="bg-white w-[64px] h-[64px] rounded-full flex justify-center items-center text-[28px] mb-2">{category.icon}</div>
+              <span className="text-white">{category.title}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <h2>Anúncios</h2>
+      <div className="flex flex-wrap">
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+      </div>
+      <p className="flex self-end">Ver mais</p>
     </AdminTemplate>
   )
 };
