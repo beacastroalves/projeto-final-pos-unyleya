428cc902d50ef389add39e572433ae15d8961a6d
beacastroalves <beacastro1008@gmail.com>
2025-10-28 18:54:42 +0000

    Ajustando o card de produtos admin

--- src/components/card-product-admin/index.tsx
+++ src/components/card-product-admin/index.tsx
@@ -1,4 +1,4 @@
-import { PiPencilLineFill, PiTrashBold } from "react-icons/pi";
+import { BiEditAlt, BiTrash } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 
 const CardProductAdmin = () => {
@@ -6,7 +6,10 @@ const CardProductAdmin = () => {
   const navigate = useNavigate();
 
   return (
-    <button onClick={() => navigate("/products/details")} className="flex flex-col gap-2 justify-center items-center px-6 py-8 my-4 shadow-md rounded-md h-fit w-fit min-w-[220px] border-2 border-gray-50 bg-white cursor-pointer">
+    <button
+    // onClick={() => navigate("/products/details")}
+      className="flex flex-col gap-2 justify-center items-center px-6 py-8 my-4 shadow-md rounded-md h-fit w-fit min-w-[220px] border-2 border-gray-50 bg-white cursor-pointer"
+    >
       <h1 className="text-[16px] font-bold mb-1 w-full text-left">Nome do Produto</h1>
 
       <img className="w-[150px] my-3" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthegoodguys.sirv.com%2Fproducts%2F50083302%2F50083302_833214.PNG%3Fscale.height%3D505%26scale.width%3D773%26canvas.height%3D505%26canvas.width%3D773%26canvas.opacity%3D0%26q%3D90&f=1&nofb=1&ipt=261965353f8013573740608f6d87e8b15b16a963971a36fe5c271f19a9130610" alt="Alexa dot 8 geracao" />
@@ -17,11 +20,11 @@ const CardProductAdmin = () => {
           <p className="w-full text-[20px] text-left">R$ 000,00</p>
         </div>
         <div className="flex flex-col items-center gap-1 mt-2 text-[18px]">
-          <button onClick={() => alert("cliquei em editar")} className="cursor-pointer p-1 transition duration-200 hover:text-primary hover:scale-140">
-            <PiPencilLineFill />
+          <button onClick={() => navigate("/form-product")} className="cursor-pointer p-1 transition duration-200 hover:text-primary hover:scale-150" title="Editar Produto">
+            <BiEditAlt />
           </button>
-          <button onClick={() => alert("cliquei em apagar")} className="cursor-pointer p-1 transition duration-200 hover:text-red-700 hover:scale-140">
-            <PiTrashBold />
+          <button onClick={() => alert("cliquei em apagar")} className="cursor-pointer p-1 transition duration-200 hover:text-red-700 hover:scale-150" title="Deletar Produto">
+            <BiTrash />
           </button>
         </div>
       </div>
