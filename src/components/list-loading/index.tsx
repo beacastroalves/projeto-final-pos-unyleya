1bc0408e466b7cdfd8b753979f1a18629a993372
beacastroalves <beacastro1008@gmail.com>
2025-11-07 18:13:36 +0000

    Ajuste em uma variavel

--- src/components/list-loading/index.tsx
+++ src/components/list-loading/index.tsx
@@ -5,7 +5,7 @@ const ListLoading = () => {
   return (
     <div className='flex flex-wrap'>
       {
-        Array.from({length:4}).map((item, index) => (
+        Array.from({length:4}).map((_, index) => (
           <div key={index} className="flex flex-col items-center justify-between p-6 my-4 mx-2 shadow-md rounded-md h-[400px] w-[230px] border-2 border-gray-50 bg-white cursor-pointer">
             <Skeleton width={180} height={30} />
             <Skeleton width={180} height={150} />
